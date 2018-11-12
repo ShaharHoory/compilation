2fbbce2800dea274d05b7f2518cbc38bfcb58118
Shahar Hoory <shaharhoo@gmail.com>
2018-11-12 17:29:03 +0200

    jh

--- reader.ml
+++ reader.ml
@@ -257,7 +257,7 @@ let nt_l_paren_ = disj (PC.char '(') (PC.char '[');;
 
 
 let rec _sexpr_ s = 
-let _all_parsers = PC.disj_list ( _three_dots_ ::   make_wrapped_with_junk _scientific_notation_ :: make_wrapped_with_junk _boolean_parser_ ::  make_wrapped_with_junk char_parser ::
+let _all_parsers = PC.disj_list (make_wrapped_with_junk _three_dots_ ::   make_wrapped_with_junk _scientific_notation_ :: make_wrapped_with_junk _boolean_parser_ ::  make_wrapped_with_junk char_parser ::
 				    make_wrapped_with_junk _number_ ::  make_wrapped_with_junk string_parser ::  make_wrapped_with_junk symbol_parser ::  make_wrapped_with_junk _dotted_list_parser :: make_wrapped_with_junk _squared_dotted_list_parser::
 				    make_wrapped_with_junk  _list_parser :: make_wrapped_with_junk  _squared_list_parser :: make_wrapped_with_junk _vector_parser ::  make_wrapped_with_junk _quoted_ ::  make_wrapped_with_junk _quasi_quoted_ ::
 				    make_wrapped_with_junk _unquote_spliced_ :: make_wrapped_with_junk _unquoted_ ::(*make_wrapped_with_junk  _squared_brackets_notation_ ::*)  [])
